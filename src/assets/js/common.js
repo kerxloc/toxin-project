1ef1b06963489d9c6fc7f5a954ac0a67d6b4ea57
Ruslan Gerasimov <gerasimov026@gmail.com>
2020-09-23 23:50:05 +0300

    Add search form in container and addaptive background

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -9,3 +9,13 @@ burger.onclick = function () {
     menu.classList.toggle("active");
     body.classList.toggle("lock");
 };
+
+function ibg(){
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+         }
+    }
+} 
+    ibg();
