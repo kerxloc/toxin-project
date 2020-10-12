2574991a69d87796c030afbfde5a5782163e800e
Ruslan Gerasimov <gerasimov026@gmail.com>
2020-10-12 14:43:29 +0300

    Add adaptiv for mobile

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -10,10 +10,10 @@ burger.onclick = function () {
     body.classList.toggle("lock");
 };
 
-function ibg(){
-    let ibg=document.querySelectorAll(".ibg");
+function ibg() {
+    let ibg = document.querySelectorAll(".ibg");
     for (var i = 0; i < ibg.length; i++) {
-    if(ibg[i].querySelector('img')){
+    if (ibg[i].querySelector('img')) {
     ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
          }
     }
